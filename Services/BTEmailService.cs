afb11413b5a1566362123a250ef6d4db242c9f0b
wtrinh02 <yelsew02@gmail.com>
2024-02-11 20:26:47 -0800

    Changed mail settings

--- Services/BTEmailService.cs
+++ Services/BTEmailService.cs
@@ -31,8 +31,8 @@ namespace TheBugTracker.Services
             try
             {
                 using var smtp = new SmtpClient();
-                smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
-                smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
+                smtp.Connect(_mailSettings.MailHost, _mailSettings.MailPort, SecureSocketOptions.StartTls);
+                smtp.Authenticate(_mailSettings.Mail, _mailSettings.MailPassword);
 
                 await smtp.SendAsync(email);
 
